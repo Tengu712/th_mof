35f6d9bb4f3b5590196a535a7d701aba340c176d
Tengu712 <65766435+Tengu712@users.noreply.github.com>
2022-03-20 16:46:28 +0900

    create scene update system.

--- src/gameapis/scenes.rs
+++ src/gameapis/scenes.rs
@@ -1,8 +1,31 @@
-pub mod game_scene;
-pub mod title_scene;
+/// A struct that has a function field that updates each scene and common fields that all scene need.
+pub struct Scene {
+    updater: fn(Self) -> Result<Self, String>,
+}
 
-/// A trait for scenes.
-pub trait Scene {
+impl Scene {
+    /// Constructor for user.
+    pub fn new() -> Result<Self, String> {
+        Ok(Scene {
+            updater: update_title,
+        })
+    }
     /// Update scene.
-    fn update(self: Box<Self>) -> Result<Box<dyn Scene>, String>;
+    pub fn update(self) -> Result<Self, String> {
+        (self.updater)(self)
+    }
+}
+
+/// Private function. Update title scene.
+fn update_title(_: Scene) -> Result<Scene, String> {
+    Ok(Scene {
+        updater: update_game,
+    })
+}
+
+/// Private function. Update game scene.
+fn update_game(scene: Scene) -> Result<Scene, String> {
+    Ok(Scene {
+        updater: scene.updater,
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,15 @@ pub mod winapis;
 use gameapis::scenes::*;
 use winapis::{direct2d::*, winapi::*};
 
+/// Entry point. Unwrap application error here.
 fn main() {
-    // Windows API
+    run()
+        .map_err(|e| show_messagebox(e, "Error".to_owned()))
+        .unwrap();
+}
+
+/// Run application. Defined for returning Result to main function.
+fn run() -> Result<(), String> {
     let winapp = WindowsApplication::new(
         "Window title",
         1280,
@@ -14,34 +21,18 @@ fn main() {
             "フルスクリーンで起動しますか？".to_owned(),
             "Question".to_owned(),
         ),
-    )
-    .map_err(|e| show_messagebox(e, "Error".to_owned()))
-    .unwrap();
-    let d2dapp = D2DApplication::new(&winapp)
-        .map_err(|e| show_messagebox(e, "Error".to_owned()))
-        .unwrap();
-    // Game API
-    let mut scene = title_scene::TitleScene::new()
-        .map_err(|e| show_messagebox(e, "Error".to_owned()))
-        .unwrap();
-    // Mainloop
+    )?;
+    let d2dapp = D2DApplication::new(&winapp)?;
+    let mut scene = Scene::new()?;
     loop {
         if winapp.do_event() {
             break;
         }
+        scene = scene.update()?;
         d2dapp.begin_draw();
         d2dapp.clear_screen(0.0, 0.0, 0.0);
-        scene = scene
-            .update()
-            .map_err(|e| show_messagebox(e, "Error".to_owned()))
-            .unwrap();
-        d2dapp
-            .end_draw()
-            .map_err(|e| show_messagebox(e, "Error".to_owned()))
-            .unwrap();
-        d2dapp
-            .present(1, 0)
-            .map_err(|e| show_messagebox(e, "Error".to_owned()))
-            .unwrap();
+        d2dapp.end_draw()?;
+        d2dapp.present(1, 0)?;
     }
+    Ok(())
 }
